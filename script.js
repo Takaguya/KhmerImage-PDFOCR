b9b2ca7e9a866344c708124d768a546e8c192c2d
Takaguya <106542482+Takaguya@users.noreply.github.com>
2024-12-18 19:36:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const class_labels = {
 // Create an async function to load the model
 async function loadModel() {
     try {
-        const onnxModelPath = 'models/model.onnx';
+        const onnxModelPath = 'models/modified_model.onnx';
         const session = await new onnx.InferenceSession();
         await session.loadModel(onnxModelPath);
         console.log('Model loaded successfully');
